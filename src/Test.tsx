6900ff31dbd180f383053374e08acd588ba99f2a
Jiayu Zhang <jiayuzhangg@gmail.com>
2023-05-14 21:14:17 -0400

    created project

---
+++ src/Test.tsx
@@ -0,0 +1,11 @@
+import { ReactElement } from "react";
+
+const Test = (): ReactElement => {
+  return (
+    <div>
+      test
+    </div>
+  );
+};
+
+export default Test
---
+++ src/__tests__/test.test.tsx
@@ -0,0 +1,9 @@
+import { render } from "@testing-library/react";
+import Test from "../Test";
+
+describe('<test/>', () => {
+  it('renders the test component', ()=>{
+    render(<Test/>)
+
+  })
+})
