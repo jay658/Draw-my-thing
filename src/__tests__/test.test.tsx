167126260944fe284147738f9e0d844cfc5c9be4
Jiayu Zhang <jiayuzhangg@gmail.com>
2023-05-14 21:18:00 -0400

    testing failed test

--- src/__tests__/test.test.tsx
+++ src/__tests__/test.test.tsx
@@ -1,9 +1,20 @@
-import { render } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
 import Test from "../Test";
 
 describe('<test/>', () => {
   it('renders the test component', ()=>{
-    render(<Test/>)
+    render(<Test/>);
 
+    const testText = screen.queryByText("test");
+
+    expect(testText).toBeInTheDocument();
+  })
+
+  it('this test fails', ()=>{
+    render(<Test/>);
+
+    const testText = screen.queryByText("no test");
+
+    expect(testText).toBeInTheDocument();
   })
 })
