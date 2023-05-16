55079f34b29c4aab1fea2e8edd8a2d05d4f0bb86
Jianing Zhang <jzhang718@gmail.com>
2023-05-16 17:38:04 -0400

    connected the database to server

---
+++ server/database/models/index.js
@@ -0,0 +1,60 @@
+import { Sequelize } from "sequelize";
+import configObj from "../config/config.js";
+import { fileURLToPath } from "url";
+import fs from "fs";
+import path from "path";
+import process from "process";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const basename = path.basename(__filename);
+const env = process.env.NODE_ENV || "development";
+
+const config = configObj[env];
+
+const db = {};
+
+let sequelize;
+if (config.use_env_variable) {
+  sequelize = new Sequelize(process.env[config.use_env_variable], config);
+} else {
+  sequelize = new Sequelize(
+    config.database,
+    config.username,
+    config.password,
+    config
+  );
+}
+
+fs.readdirSync(__dirname)
+  .filter((file) => {
+    return (
+      file.indexOf(".") !== 0 &&
+      file !== basename &&
+      file.slice(-3) === ".ts" &&
+      file.indexOf(".test.ts") === -1
+    );
+  })
+  .forEach((file) => {
+    const filePath = path.join(__dirname, file);
+    const model = import(/* @vite-ignore */ filePath).then((module) => {
+      const model = module.default(sequelize, Sequelize.DataTypes);
+      return model;
+    });
+    // const model = require(path.join(__dirname, file))(
+    //   sequelize,
+    //   Sequelize.DataTypes
+    // );
+    db[model.name] = model;
+  });
+
+Object.keys(db).forEach((modelName) => {
+  if (db[modelName].associate) {
+    db[modelName].associate(db);
+  }
+});
+
+db.sequelize = sequelize;
+db.Sequelize = Sequelize;
+
+export default db;
