eba6e0478594dca481052284a155bdabfc7dea77
Jiayu Zhang <jiayuzhangg@gmail.com>
2023-09-23 00:20:48 -0400

    Fixed bugs with game not found and cleaned up session code and error message code

--- server/socketCommands/helperFunctions/games.ts
+++ server/socketCommands/helperFunctions/games.ts
@@ -1,7 +1,19 @@
-import { Game, GamesT, gameInfo } from '../types'
+import { Game, GamesT, Member, gameInfo } from '../types'
+
+import { Socket } from 'socket.io'
 
 export const games: GamesT = {}
 
 export const addGame = (gameInfo: gameInfo) => {
   games[gameInfo.name] = new Game(gameInfo)
+}
+
+export const restoreSession = (socket: Socket, sessionId: string, userInfo: Member) => {
+  const { username, avatar, roomName } = userInfo
+      socket.username = username
+      socket.readyStatus = false
+      socket.avatar = avatar
+      socket.roomName = roomName
+      socket.sessionId = sessionId
+      if(roomName) socket.join(roomName)
 }
