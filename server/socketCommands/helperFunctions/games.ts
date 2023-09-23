2703909814e02dea6bcd839aa1db65d6cc53c994
Jiayu Zhang <jiayuzhangg@gmail.com>
2023-09-23 00:23:07 -0400

    commit

--- server/socketCommands/helperFunctions/games.ts
+++ server/socketCommands/helperFunctions/games.ts
@@ -1,19 +1,7 @@
-import { Game, GamesT, Member, gameInfo } from '../types'
-
-import { Socket } from 'socket.io'
+import { Game, GamesT, gameInfo } from '../types'
 
 export const games: GamesT = {}
 
 export const addGame = (gameInfo: gameInfo) => {
   games[gameInfo.name] = new Game(gameInfo)
-}
-
-export const restoreSession = (socket: Socket, sessionId: string, userInfo: Member) => {
-  const { username, avatar, roomName } = userInfo
-      socket.username = username
-      socket.readyStatus = false
-      socket.avatar = avatar
-      socket.roomName = roomName
-      socket.sessionId = sessionId
-      if(roomName) socket.join(roomName)
 }
